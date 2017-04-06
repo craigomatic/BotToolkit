aa1b5808bead3a7357c9d4c77a3b95086bbc16c3
Craig Presti <craig@seventhcap.com>
2017-04-06 12:04:10 -0700

    Added a boolean to enable sending the typing indicator automatically

--- src/BotToolkit/BotService.cs
+++ src/BotToolkit/BotService.cs
@@ -17,14 +17,30 @@ namespace BotToolkit
 
         public IAuthenticator Authenticator { get; set; }
 
+        /// <summary>
+        /// When true, will always send the typing indicator prior to the bot responding
+        /// </summary>
+        public bool SendTypingIndicator { get; set; }
+
         public BotService(Bot bot)
         {
             this.Bot = bot;
+            this.SendTypingIndicator = true;
         }
 
-        public Task RespondAsync(IMessageActivity activity)
+        public async Task RespondAsync(IMessageActivity activity)
         {
-            return Conversation.SendAsync(activity, () => Chain.From(() => new MenuDialog(this.Bot.RootMenu, this.Authenticator)));
+            if (activity.Type == ActivityTypes.Message
+                && this.SendTypingIndicator)
+            {
+                var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                var reply = (activity as Activity).CreateReply();
+                reply.Type = ActivityTypes.Typing;
+
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+
+            await Conversation.SendAsync(activity, () => Chain.From(() => new MenuDialog(this.Bot.RootMenu, this.Authenticator)));
         }
     }
 }
