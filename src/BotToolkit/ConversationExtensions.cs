09d05bcff6c0ade622c72b694b0385e366c1c6f8
Craig Presti <craig@seventhcap.com>
2017-04-15 17:54:35 -0700

    Add message text optional param and ToThumbnailList extension

--- src/BotToolkit/ConversationExtensions.cs
+++ src/BotToolkit/ConversationExtensions.cs
@@ -11,12 +11,27 @@ namespace BotToolkit
 {
     public static class ConversationExtensions
     {
-        public static IMessageActivity ToThumbnailCarousel(this IDialogContext context, IEnumerable<ICarouselItem> items)
+        public static IMessageActivity ToThumbnailCarousel(this IDialogContext context, IEnumerable<ICarouselItem> items, string messageText = null)
+        {
+            var msg = _BuildMessage(context, items, messageText);
+            msg.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+            return msg;
+        }
+
+        public static IMessageActivity ToThumbnailList(this IDialogContext context, IEnumerable<ICarouselItem> items, string messageText = null)
+        {
+            var msg = _BuildMessage(context, items, messageText);
+            msg.AttachmentLayout = AttachmentLayoutTypes.List;
+            return msg;
+        }
+
+        private static IMessageActivity _BuildMessage(IDialogContext context, IEnumerable<ICarouselItem> items, string messageText)
         {
             var msg = context.MakeMessage();
+            msg.Text = messageText;
 
             foreach (var item in items)
-            {               
+            {
                 msg.Attachments.Add(new ThumbnailCard
                 {
                     Title = item.Title,
